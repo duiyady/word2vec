54f15b6b31146b86a57f61d9ea81d08617b395f8
duiyady@163.com <duiyajava1997>
2020-11-24 23:58:35 +0800

    init

--- src/main/com/duiya/model/Word2Vec.java
+++ src/main/com/duiya/model/Word2Vec.java
@@ -1,6 +1,11 @@
 package com.duiya.model;
 
 public class Word2Vec {
+    private boolean CBOW = true;
+    private boolean HAFUMAN_TREE = false;
+    private int window, min_count, min_reduce;
+    private long vocab_max_size;
+    private long vocab_size;
     public static void main(String[] args) {
         System.out.println("hello world");
     }
