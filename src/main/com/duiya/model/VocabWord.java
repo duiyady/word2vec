da63ba3005aaadb81757b4b3f9048fde02b76745
duiya <duiyady@163.com>
2020-11-28 17:21:10 +0800

    init

---
+++ src/main/com/duiya/model/VocabWord.java
@@ -0,0 +1,54 @@
+package com.duiya.model;
+
+import java.util.List;
+
+public class VocabWord {
+    private long count; // 出现的次数
+    private List<Integer> point;  //路径
+    private String word;  //词
+    private List<Integer> code; //哈夫曼编码
+    private int codelen;
+
+    public VocabWord() {
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
+
+    public List<Integer> getPoint() {
+        return point;
+    }
+
+    public void setPoint(List<Integer> point) {
+        this.point = point;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public List<Integer> getCode() {
+        return code;
+    }
+
+    public void setCode(List<Integer> code) {
+        this.code = code;
+    }
+
+    public int getCodelen() {
+        return codelen;
+    }
+
+    public void setCodelen(int codelen) {
+        this.codelen = codelen;
+    }
+}
--- src/main/com/duiya/model/Word2Vec.java
+++ src/main/com/duiya/model/Word2Vec.java
@@ -3,9 +3,17 @@ package com.duiya.model;
 public class Word2Vec {
     private boolean CBOW = true;
     private boolean HAFUMAN_TREE = false;
+    private int MAX_STRING = 100;
+    private int EXP_TABLE_SIZE = 1000;
+    private int MAX_EXP = 6;
+    private int MAX_SENTENCE_LENGTH = 1000;
+    private int MAX_CODE_LENGTH = 40;
     private int window, min_count, min_reduce;
     private long vocab_max_size;
     private long vocab_size;
+    private int vacab_hash_size = 30000000;
+
+
     public static void main(String[] args) {
         System.out.println("hello world");
     }
