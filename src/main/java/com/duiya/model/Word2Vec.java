6067c02c9e37b924d2ba879886c6eeb57afd2a7c
duiya <duiyady@163.com>
2020-12-18 10:50:31 +0800

    base over

--- src/main/java/com/duiya/model/Word2Vec.java
+++ src/main/java/com/duiya/model/Word2Vec.java
@@ -10,22 +10,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Word2Vec {
-    private boolean HAFUMAN = true;
+    private boolean HAFUMAN = false;
     private boolean CBOW = true;
     private int EMBEDDING_SIZE = 128;
     private VocabList vocabList;
     private ExpTable expTable;
-    private float learnRate = 0.00005f;
+    private float learnRate = 0.0005f;
     private int negNum = 10;  //负采样个数
+    private int window = 2;
+    private int DEFAULT_INDEX; // 默认
 
     public Word2Vec() {
         this.expTable = new ExpTable();
-        String fileName = WordSplitUtil.class.getClassLoader().getResource("train.txt").getPath();
         this.vocabList = new VocabList();
-        createVocab(fileName);
         this.vocabList.add("</default>");
         vocabList.sortVocab();
+        DEFAULT_INDEX = this.vocabList.searchVocab("</default>");
+    }
 
+    public void init(){
         if (this.HAFUMAN){ //哈夫曼
             vocabList.initEmbedding(this.EMBEDDING_SIZE);
             vocabList.initHiddenEmbedding(this.EMBEDDING_SIZE, 1);
@@ -35,21 +38,128 @@ public class Word2Vec {
             vocabList.initHiddenEmbedding(this.EMBEDDING_SIZE, 0);
             vocabList.initNegativeSamplingTable();
         }
-
-
-        if(this.CBOW){ // cbow
-
-        }else { // skip-gram
-
-        }
     }
 
     public static void main(String[] args) {
         Word2Vec model = new Word2Vec();
+        String fileName = WordSplitUtil.class.getClassLoader().getResource("train.txt").getPath();
+        model.createVocab(fileName);
+        model.init();
+        model.train(fileName, 99999);
+        List<String> tt = model.getMaxRelation("唐朝", 10);
+        System.out.println(tt);
+        tt = model.getMaxRelation("韩信", 10);
+        System.out.println(tt);
+        tt = model.getMaxRelation("曹操", 10);
+        System.out.println(tt);
+
+
+
 
     }
 
-    public float train(Integer center, ArrayList<Integer> neighbor){
+    public List<String> getMaxRelation(String word, int top){
+        List<String> words = new ArrayList<String>(top);
+        List<Float> value = new ArrayList<Float>();
+
+        int centerIndex = this.vocabList.searchVocab(word);
+        if(centerIndex == -1){
+            return null;
+        }
+        float[] center = this.vocabList.getEmbedding()[centerIndex];
+        for(int i = 0; i < this.vocabList.size(); i++){
+            if(i != centerIndex){
+                float tmp = 0;
+                float[] nei = this.vocabList.getEmbedding()[i];
+                for(int j = 0; j < this.EMBEDDING_SIZE; j++){
+                    tmp += center[j]*nei[j];
+                }
+                if(value.size() == 0){
+                    value.add(tmp);
+                    words.add(this.vocabList.get(i).getWord());
+                }else {
+                    boolean flag = false;
+                    int m = 0;
+                    for(; m < value.size(); m++){
+                        if(tmp > value.get(m)){
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if(flag == false){
+                        value.add(tmp);
+                        words.add(this.vocabList.get(i).getWord());
+                    }else {
+                        value.add(m, tmp);
+                        words.add(m, this.vocabList.get(i).getWord());
+                    }
+                    while (value.size() > top){
+                        value.remove(value.size()-1);
+                        words.remove(words.size()-1);
+                    }
+                }
+            }
+        }
+        return words;
+    }
+
+    public void train(String fileName, int epoch){
+        JiebaSegmenter jiebaSegmenter = new JiebaSegmenter();
+        int noWord = this.vocabList.searchVocab("</default>");
+        float loss = 0;
+        int count = 1;
+        for(int epo = 0; epo < epoch; epo++){
+            FileReader fileReader = null;
+            BufferedReader bufferedReader = null;
+            try {
+                File file = new File(fileName);
+                fileReader = new FileReader(file);
+                bufferedReader = new BufferedReader(fileReader);
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    line = line.replaceAll("(?i)[^a-zA-Z0-9\u4E00-\u9FA5]", "");
+                    List<String> strings = jiebaSegmenter.sentenceProcess(line);
+                    strings.add(0, "</s>");
+                    for(int i = 1; i < strings.size(); i++){
+                        int center = this.vocabList.searchVocab(strings.get(i));
+                        if (center == -1){
+                            continue;
+                        }
+                        List<Integer> neighbor = new ArrayList<Integer>();
+                        int start = Math.max(i - this.window, 0);
+                        int end = Math.min(i + this.window, strings.size()-1);
+                        if(end == strings.size()-1){
+                            start = Math.max(strings.size() - 4, 0);
+                        }
+                        if(start != end){
+                            for(int j = start; j <= end; j++){
+                                if(j != i) {
+                                    int index = this.vocabList.searchVocab(strings.get(j));
+                                    if (index == -1) {
+                                        index = noWord;
+                                    }
+                                    neighbor.add(index);
+                                }
+                            }
+                            loss += this.train(center, neighbor);
+                            count += 1;
+                            if(count%1000 == 0){
+                                System.out.println(loss/count);
+                                loss = 0;
+                                count = 1;
+                            }
+                        }
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+                System.exit(-1);
+            }
+        }
+    }
+
+
+    public float train(Integer center, List<Integer> neighbor){
         if(this.CBOW){// cbow
             float[] sumEmbedding = new float[this.EMBEDDING_SIZE];
             float loss = 0;
@@ -257,8 +367,7 @@ public class Word2Vec {
         }
     }
 
-    public void createVocab(String path){
-        String fileName = WordSplitUtil.class.getClassLoader().getResource("train.txt").getPath();
+    public void createVocab(String fileName){
         FileReader fileReader = null;
         BufferedReader bufferedReader = null;
         try {
@@ -267,8 +376,7 @@ public class Word2Vec {
             bufferedReader = new BufferedReader(fileReader);
             JiebaSegmenter jiebaSegmenter = new JiebaSegmenter();
 
-            String line = "";
-            int nowID = 0;
+            String line;
             while ((line = bufferedReader.readLine()) != null) {
                 line = line.replaceAll("(?i)[^a-zA-Z0-9\u4E00-\u9FA5]", "");
                 List<String> strings = jiebaSegmenter.sentenceProcess(line);
